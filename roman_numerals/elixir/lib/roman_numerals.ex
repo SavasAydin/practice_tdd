6683d7581a57cb171a302036ca0f88e45e4b7e9c
Savas Aydin <savayd@Savass-MacBook-Pro.local>
2016-03-14 20:46:21 +0100

    expresses the numbers up to 5000 in roman numerical system

--- roman_numerals/elixir/lib/roman_numerals.ex
+++ roman_numerals/elixir/lib/roman_numerals.ex
@@ -3,24 +3,28 @@ defmodule Roman_numerals do
 	def convert(0) do
 		""
 	end
-	def convert(x) when x < 90 do
+	def convert(x) when x < 5000 do
 		l = get_low_limit(x)
-		re = get_roman_expression(l)
+		re = roman_expression_of(l)
 		re <> convert(x-l)
 	end
 
 	defp get_low_limit(x) do
-		limits = [{1,4}, {4,5}, {5,9}, {9,10}, {10,40}, {40,50}, {50, 90}]
-		[{low, _}] = Enum.filter(limits, fn({l,h}) -> x >= l and x < h end)
-		low
+		limits = all_limits()
+		[{low_limit, _}] = Enum.filter(limits, fn({l,h}) -> x >= l and x < h end)
+		low_limit
 	end
 
-	defp get_roman_expression(x) do
+	defp roman_expression_of(x) do
 		rns = roman_numerical_system()
 		{^x, re} = List.keyfind(rns, x, 0)
 		re
 	end
 
+	defp all_limits() do
+		[{1,4}, {4,5}, {5,9}, {9,10}, {10,40}, {40,50}, {50,90}, {90,100}, {100,500},{500,900}, {900,1000}, {1000,5000}]
+	end
+	
 	defp roman_numerical_system() do
 		[{1, "I"},
 		 {4,"IV"},
@@ -28,6 +32,11 @@ defmodule Roman_numerals do
 		 {9, "IX"},
 		 {10, "X"},
 		 {40, "XL"},
-		 {50, "L"}]
+		 {50, "L"},
+		 {90, "XC"},
+		 {100, "C"},
+		 {500, "D"},
+		 {900, "CM"},
+		 {1000, "M"}]
 	end
 end
--- roman_numerals/elixir/test/roman_numerals_test.exs
+++ roman_numerals/elixir/test/roman_numerals_test.exs
@@ -1,72 +1,80 @@
 defmodule RomanNumeralsTest do
-  use ExUnit.Case
+	use ExUnit.Case
 
-  test "one_is_expressed_as_I" do
-    assert Roman_numerals.convert(1) == "I"
-  end
+	test "one_is_expressed_as_I" do
+		assert Roman_numerals.convert(1) == "I"
+	end
 
-  test "two_is_expressed_as_II" do
-    assert Roman_numerals.convert(2) == "II"
-  end
+	test "two_is_expressed_as_II" do
+		assert Roman_numerals.convert(2) == "II"
+	end
 
-  test "three_is_expressed_as_III" do
-    assert Roman_numerals.convert(3) == "III"
-  end
+	test "three_is_expressed_as_III" do
+		assert Roman_numerals.convert(3) == "III"
+	end
 
-  test "four_is_expressed_as_IV" do
-    assert Roman_numerals.convert(4) == "IV"
-  end
-	
-  test "five_is_expressed_as_V" do
-    assert Roman_numerals.convert(5) == "V"
-  end
+	test "four_is_expressed_as_IV" do
+		assert Roman_numerals.convert(4) == "IV"
+	end
+
+	test "five_is_expressed_as_V" do
+		assert Roman_numerals.convert(5) == "V"
+	end
 
 	test "six_is_expressed_as_VI" do
-    assert Roman_numerals.convert(6) == "VI"
-  end
+		assert Roman_numerals.convert(6) == "VI"
+	end
 
 	test "nine_is_expressed_as_IX" do
-    assert Roman_numerals.convert(9) == "IX"
-  end
+		assert Roman_numerals.convert(9) == "IX"
+	end
 
 	test "ten_is_expressed_as_X" do
-    assert Roman_numerals.convert(10) == "X"
-  end
-
-	test "fourteen_is_expressed_as_XIV" do
-    assert Roman_numerals.convert(14) == "XIV"
-  end
-
-	test "twenty_eight_is_expressed_as_XXVIII" do
-    assert Roman_numerals.convert(28) == "XXVIII"
-  end
+		assert Roman_numerals.convert(10) == "X"
+	end
 
 	test "thirty_nine_is_expressed_as_XXXIX" do
-    assert Roman_numerals.convert(39) == "XXXIX"	
-  end
+		assert Roman_numerals.convert(39) == "XXXIX"
+	end
 
 	test "fourty_is_expressed_as_XL" do
-    assert Roman_numerals.convert(40) == "XL"	
-  end
+		assert Roman_numerals.convert(40) == "XL"
+	end
 
 	test "fourty_four_is_expressed_as_XLIV" do
-    assert Roman_numerals.convert(44) == "XLIV"	
-  end
+		assert Roman_numerals.convert(44) == "XLIV"
+	end
 
 	test "fifty_is_expressed_as_L" do
-    assert Roman_numerals.convert(50) == "L"	
-  end
+		assert Roman_numerals.convert(50) == "L"
+	end
 
-	test "sixty_is_expressed_as_LX" do
-    assert Roman_numerals.convert(60) == "LX"	
-  end
+	test "ninety_is_expressed_as_XC" do
+		assert Roman_numerals.convert(90) == "XC"
+	end
 
-	test "seventy_is_expressed_as_LXX" do
-    assert Roman_numerals.convert(70) == "LXX"	
-  end
+	test "hundred_is_expressed_as_C" do
+		assert Roman_numerals.convert(100) == "C"
+	end
 
-	test "eighty_nine_is_expressed_as_LXXXIX" do
-    assert Roman_numerals.convert(89) == "LXXXIX"	
-  end
-	
+	test "three_hundred_sixty_nine_is_expressed_as_CCCLXIX" do
+		assert Roman_numerals.convert(369) == "CCCLXIX"
+	end
+
+	test "five_hundred_is_expressed_as_D" do
+		assert Roman_numerals.convert(500) == "D"
+	end
+
+	test "nine_hundred_is_expressed_as_CM" do
+		assert Roman_numerals.convert(900) == "CM"
+	end
+		
+	test "thousand_is_expressed_as_M" do
+		assert Roman_numerals.convert(1000) == "M"
+	end
+
+	test "four_thousand_nine_hundred_ninety_nine_is_expressed_as_MMMMCMXCIX" do
+		assert Roman_numerals.convert(4999) == "MMMMCMXCIX"
+	end			
+
 end
